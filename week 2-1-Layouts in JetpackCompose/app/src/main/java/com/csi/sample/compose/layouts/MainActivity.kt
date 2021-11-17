5257b00bf3c35363cb30dfd90596e146b91f09da
Robin <haveahopes@gmail.com>
2021-11-17 22:56:17 +0900

    #15 코드랩3번쨰 적용 중

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/MainActivity.kt
@@ -3,10 +3,15 @@ package com.csi.sample.compose.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.csi.sample.compose.layouts.ui.theme.ComposeLayoutsTheme
 
@@ -34,5 +39,23 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeLayoutsTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    ComposeLayoutsTheme {
+        PhotographerCard()
     }
 }
