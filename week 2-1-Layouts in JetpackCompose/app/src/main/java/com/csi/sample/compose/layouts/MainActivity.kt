537b685c3403fa11bbab792582f6aa200503c76f
Robin <haveahopes@gmail.com>
2021-11-21 20:09:42 +0900

    ImageList 추가

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/MainActivity.kt
@@ -3,10 +3,12 @@ package com.csi.sample.compose.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +17,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -28,13 +31,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.csi.sample.compose.layouts.ui.theme.ComposeLayoutsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -161,5 +167,49 @@ fun LazyList() {
         items(100) {
             Text("Item $it")
         }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.size(50.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Row() {
+        Button(onClick = {
+            coroutineScope.launch {
+                scrollState.animateScrollToItem(0)
+            }
+        }) {
+            Text("Scroll to the top")
+        }
+
+        Button(onClick = {
+            coroutineScope.launch {
+                scrollState.animateScrollToItem(listSize - 1)
+            }
+        }) {
+            Text("Scroll to the top")
+        }
+    }
+    LazyColumn(state = scrollState) {
+        items(listSize) {
+            ImageListItem(it)
+        }
+
     }
 }
