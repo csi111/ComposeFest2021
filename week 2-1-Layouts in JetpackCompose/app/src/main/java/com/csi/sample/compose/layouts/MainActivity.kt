7fb0f865a0b17b45610b342db25a7ae020309c5a
Robin <haveahopes@gmail.com>
2021-11-22 03:08:06 +0900

    Chip 추가 및 스크롤 적용

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/MainActivity.kt
@@ -3,14 +3,18 @@ package com.csi.sample.compose.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
@@ -18,6 +22,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -35,11 +40,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+import com.csi.sample.compose.layouts.ui.layout.StaggeredGrid
 import com.csi.sample.compose.layouts.ui.modifier.firstBaselineToTop
 import com.csi.sample.compose.layouts.ui.theme.ComposeLayoutsTheme
 import kotlinx.coroutines.launch
@@ -95,13 +103,18 @@ fun LayoutsCodelab() {
     }
 }
 
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
 @Composable
 private fun BodyContent(modifier: Modifier = Modifier) {
-    MyOwnColumn(modifier = modifier.padding(8.dp)) {
-        Text(text = "MyOwnColumn")
-        Text(text = "place items")
-        Text(text = "vertically.")
-        Text(text = "We've done it by hand!")
+    StaggeredGrid(modifier = modifier.horizontalScroll(rememberScrollState()), rows = 5) {
+        for (topic in topics) {
+            Chip(text = topic, modifier = Modifier.padding(8.dp))
+        }
     }
 }
 
@@ -261,3 +274,33 @@ fun MyOwnColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) 
         }
     }
 }
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    ComposeLayoutsTheme {
+        Chip(text = "Hi, there")
+    }
+}
+
---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/csi/sample/compose/layouts/ui/layout/StaggedLayout.kt
@@ -0,0 +1,53 @@
+package com.csi.sample.compose.layouts.ui.layout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measureables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measureables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width =
+            rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.maxWidth
+
+        val height =
+            rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(x = rowX[row], y = rowY[row])
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
